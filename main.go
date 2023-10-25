25a698e822cf20be03dda41a224aad68279bb8e5
Kostas Kollias <kostas.kollias.gr@gmail.com>
2023-10-25 23:11:47 +0300

    adding watch in the taskfile and a simple html page

--- main.go
+++ main.go
@@ -5,16 +5,38 @@ import (
 	"log"
 	"log/slog"
 	"net/http"
+	"text/template"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
-	slog.Info("Starting server")
+	slog.Info("Starting server!")
 
 	homeHandler := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, r.Method+": Hello world")
 	}
 
-	http.HandleFunc("/", homeHandler)
+	fileHandler := func(w http.ResponseWriter, r *http.Request) {
+
+		data := map[string][]Film{
+			"Films": {
+				{Title: "Film1", Director: "Director1"},
+				{Title: "Film2", Director: "Director2"},
+				{Title: "Film3", Director: "Director3"},
+			},
+		}
+
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, data)
+
+	}
+
+	http.HandleFunc("/", fileHandler)
+	http.HandleFunc("/io/write-string", homeHandler)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
